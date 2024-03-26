4d922416d9619eee3ab8f366979e9038d547049e
Jaspreet <js724415@gmail.com>
2024-03-26 12:30:31 -0700

    Student components seprated

---
+++ src/components/StudentList/StudentList.js
@@ -0,0 +1,15 @@
+import './StudentList.scss'
+import Student from '../Student/Student';
+
+const StudentList = () => {
+    return (
+        <section className="student-list">
+            <Student />
+            <Student />
+            <Student />
+
+        </section>
+    );
+}
+
+export default StudentList
