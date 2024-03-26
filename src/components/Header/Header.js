e29a72b090f95e22466303095fc6908653f60ccd
Jaspreet <js724415@gmail.com>
2024-03-26 13:02:53 -0700

    completed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,12 @@
 import './Header.scss'
 
 const Header=()=>{
+    const cohortNickname ='Coolest'
     return(
         <>
             <header className="header">
                 <div className="header__links header__links--left">
-                    <div className="header__logo">Cohort Directory</div>
+                    <div className="header__logo"> {cohortNickname} Cohort Directory</div>
                 </div>
                 <nav className="header__links header__links--right">
                     <a href="/">Home</a>
--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,12 +1,12 @@
 import './Student.scss'
-
+import studentImage from '../../assets/images/student-placeholder.png'
 const Student = () =>{
     return(
         <div className="student">
         <div className="student__content">
             <img 
                 className="student__image" 
-                src="https://img.icons8.com/ios/100/000000/test-account.png" 
+                src={studentImage}
                 alt="Test Student Account Image" />
         </div>
         <h4 className="student__title">Student Name</h4>
